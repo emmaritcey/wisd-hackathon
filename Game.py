1cc2c55324926754c1fb2d7843977611de56ce9a
Emma Ritcey <ritcey.emma@gmail.com>
2023-07-05 14:42:46 -0400

    add transition class

--- Game.py
+++ Game.py
@@ -5,26 +5,30 @@ import json
 
 class Game:
     def __init__(self, game_id):
-        self.id = game_id
+        self.game_id = game_id
+        self.pbp_df = self.get_playbyplay()
+        self.events_df = self.get_events()
+        self.tracking_df = self.get_tracking()
+        self.event_pbp_df = self.merge_events_with_pbp()
         
         
     def get_playbyplay(self):
-        pbp_df = playbyplay.PlayByPlay(self.id).get_data_frames()[0]
+        pbp_df = playbyplay.PlayByPlay(self.game_id).get_data_frames()[0]
         return pbp_df
     
     def get_events(self):
-        game_id = self.id
+        game_id = self.game_id
         events_df = load_game_file('data/games/' + game_id + '/' + game_id + '_events.jsonl')
         return events_df
     
     def get_tracking(self):
-        game_id = self.id
+        game_id = self.game_id
         tracking_df = load_game_file('data/games/' + game_id + '/' + game_id + '_tracking.jsonl')
         return tracking_df
         
     def merge_events_with_pbp(self):
-        pbp_df = self.get_playbyplay()
-        events_df = self.get_events()
+        pbp_df = self.pbp_df
+        events_df = self.events_df
         event_pbp_df = events_df.merge(pbp_df, left_on="pbpId", right_on="EVENTNUM", how="left")
         return event_pbp_df
     
@@ -33,12 +37,12 @@ class Game:
         get the metadata for the game from the metadata/games.json file
         add the name's of the teams 
         '''
-        game_id = self.id
+        game_id = self.game_id
         all_games_md = load_metadata('data/metadata/games.json')
         all_games_md = all_games_md['games']
         game_md = list(filter(lambda game: game['nbaId'] == game_id, all_games_md))[0]
     
         game_md['awayTeamName'] = get_team_name(game_md['awayTeamId'])
         game_md['homeTeamName'] = get_team_name(game_md['homeTeamId'])
-        
+
         return game_md
--- Transition.py
+++ Transition.py
@@ -1,13 +1,33 @@
 from Game import Game
 from src.utils.data_helpers import get_data_DREB, get_data_liveTO, get_data_FGM
+import pandas as pd
 
 class Transition(Game):
     def __init__(self, game_id, team):
         Game.__init__(self, game_id)
         self.team = team #'home' or 'away'
+        self.event_trans_opp, self.tracking_trans_opp = self.get_trans_opp()
         
         
     def get_trans_opp(self):
-        pbp_df = self.get_playbyplay()
+        '''
+        Get all transition opportunities for specified team in Game
+        '''
+        team = self.team
+        tracking_df = self.tracking_df
+        event_pbp_df = self.event_pbp_df
         
-        #get_data_liveTO(event_pbp_df, tracking_df, team)
+        event_Dreb, tracking_Dreb = get_data_DREB(event_pbp_df, tracking_df, team)
+        
+        if team == 'home': #if looking for home team's trans opps, then need away team's TO's and made Fg's, and vice versa
+            use_team = 'away'
+        else:
+            use_team = 'home'
+        event_liveTO, tracking_liveTO = get_data_liveTO(event_pbp_df, tracking_df, use_team)
+        event_FGM, tracking_FGM = get_data_FGM(event_pbp_df, tracking_df, use_team)
+
+        
+        event_trans_opp = pd.concat([event_liveTO, event_FGM, event_Dreb])
+        tracking_trans_opp = pd.concat([tracking_liveTO, tracking_FGM, tracking_Dreb])
+        
+        return event_trans_opp, tracking_trans_opp
