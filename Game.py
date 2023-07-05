7b48a79cdf42fb452b01c90c03f41781d98d3f0b
Emma Ritcey <ritcey.emma@gmail.com>
2023-07-05 13:04:07 -0400

    add merge_events_pbp function to Game class

--- Game.py
+++ Game.py
@@ -20,4 +20,9 @@ class Game:
         tracking_df = load_file('data/games/' + game_id + '/' + game_id + '_tracking.jsonl')
         return tracking_df
         
+    def merge_events_with_pbp(self):
+        pbp_df = self.get_playbyplay()
+        events_df = self.get_events()
+        event_pbp_df = events_df.merge(pbp_df, left_on="pbpId", right_on="EVENTNUM", how="left")
+        return event_pbp_df
     
